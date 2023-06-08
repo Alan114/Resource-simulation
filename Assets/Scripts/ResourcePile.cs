c44ee36736ec732681784bafb21cefc7b4db36e4
Alan Pecenkovic <alanquad@yahoo.com>
2023-06-08 16:54:01 +0200

    makng a private field variable that stores the data exposed by public property

--- Assets/Scripts/ResourcePile.cs
+++ Assets/Scripts/ResourcePile.cs
@@ -10,7 +10,13 @@ public class ResourcePile : Building
 {
     public ResourceItem Item;
 
-    public float ProductionSpeed = 0.5f;
+    private float m_ProductionSpeed = 0.5f;
+    public float ProductionSpeed
+    {
+        get { return m_ProductionSpeed; } // getter returns backing field
+        set
+        { m_ProductionSpeed = value; } // setter uses backing field
+    }
 
     private float m_CurrentProduction = 0.0f;
 
@@ -23,18 +29,18 @@ public class ResourcePile : Building
 
             m_CurrentProduction = m_CurrentProduction - amountToAdd + leftOver;
         }
-        
+
         if (m_CurrentProduction < 1.0f)
         {
-            m_CurrentProduction += ProductionSpeed * Time.deltaTime;
+            m_CurrentProduction += m_ProductionSpeed * Time.deltaTime;
         }
     }
 
     public override string GetData()
     {
-        return $"Producing at the speed of {ProductionSpeed}/s";
-        
+        return $"Producing at the speed of {m_ProductionSpeed}/s";
+
     }
-    
-    
+
+
 }
